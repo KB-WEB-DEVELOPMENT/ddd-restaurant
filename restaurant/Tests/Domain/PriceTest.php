82948997df74e3a0285098a853d04e7c69523180
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-05-01 00:10:18 +0200

    Update PriceTest.php

--- restaurant/Tests/Domain/PriceTest.php
+++ restaurant/Tests/Domain/PriceTest.php
@@ -9,86 +9,83 @@ use PHPUnit\Framework\TestCase;
 
 final class PriceTest extends TestCase
 {
-	
-	public function testIntPrice(): void
-    {
-		$price = new Price(12345); 
+   public function testIntPrice(): void
+   {
+     $price = new Price(12345); 
 		
-		$this->assertContainsOnlyInstancesOf(Price::class,[$price]);		
-    }
+     $this->assertContainsOnlyInstancesOf(Price::class,[$price]);		
+   }
 
-    public function testFloatPrice(): void
-    {
-		$price = new Price('12.345');
+   public function testFloatPrice(): void
+   {
+     $price = new Price('12.345');
 		
-		$this->assertContainsOnlyInstancesOf(Price::class,[$price]);
+     $this->assertContainsOnlyInstancesOf(Price::class,[$price]);
     }
 	
-	public function testWrongTypePrice(): void
-    {
-		$this->expectException(InvalidArgumentException::class);
+   public function testWrongTypePrice(): void
+   {
+      $this->expectException(InvalidArgumentException::class);
 		 
-		$wrongType = []; 
+      $wrongType = []; 
 		
-		$wrongType = [1,2,3]; 
+      $wrongType = [1,2,3]; 
 		
-		$price = new Price($wrongType);
+      $price = new Price($wrongType);
 	 
     }	
 	
-	public function testToInt(): void
+    public function testToInt(): void
     {
-		$price = new Price('12.345');
-		$int = $price->toInt();
-		$this->assertSame(12345,$int);
+        $price = new Price('12.345');
+	$int = $price->toInt();
+	$this->assertSame(12345,$int);
     }
 	 
-	public function testFormatted(): void
+    public function testFormatted(): void
     {
-		$price = new Price('12.346');
-		$formatted = $price->formatted();
-		$this->assertSame('EUR 12.35',$formatted);
+        $price = new Price('12.346');
+	$formatted = $price->formatted();
+	$this->assertSame('EUR 12.35',$formatted);
     }
 	
-	public function testAdd(): void
+    public function testAdd(): void
     {
-		$price = new Price('3.51');
-		$res = $price->add('2.23');
-		$int = $res->toInt(); 
-		$this->assertSame(574,$int);
+        $price = new Price('3.51');
+	$res = $price->add('2.23');
+	$int = $res->toInt(); 
+	$this->assertSame(574,$int);
     }
 	 
-	public function testSubtract(): void
+    public function testSubtract(): void
     {
-		$price = new Price(12345);
-		$res = $price->subtract(2);
-		$int = $res->toInt(); 
-		$this->assertSame(12343,$int);
+	$price = new Price(12345);
+	$res = $price->subtract(2);
+	$int = $res->toInt(); 
+	$this->assertSame(12343,$int);
     }
 	 
-	public function testMultiply(): void
+    public function testMultiply(): void
     {
-		$price = new Price('9.99');
-		$res = $price->multiplyBy('2.85');
-		$int = $res->toInt(); 
-		$this->assertSame(284715,$int);
-     
+        $price = new Price('9.99');
+	$res = $price->multiplyBy('2.85');
+	$int = $res->toInt(); 
+	$this->assertSame(284715,$int);
     }
 	 
-	public function testAddFoodVat(): void
+    public function testAddFoodVat(): void
     {
-		$price = new Price(1.98);
-		$res = $price->withFoodVat();
-		$int = $res->toInt();
-		$this->assertSame(3762,$int);		
+      $price = new Price(1.98);
+      $res = $price->withFoodVat();
+      $int = $res->toInt();
+      $this->assertSame(3762,$int);		
     }
 	 
-	public function testAddDrinkVat(): void
+    public function testAddDrinkVat(): void
     {
-		$price = new Price(1.75);
-		$res = $price->withDrinkVat();
-		$int = $res->toInt();
-		$this->assertSame(3325,$res);
-    }
-	 
+       $price = new Price(1.75);
+       $res = $price->withDrinkVat();
+       $int = $res->toInt();
+       $this->assertSame(3325,$res);
+    }	 
 }
