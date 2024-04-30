1d4596d9733a0eba1b61bcd0fdd5ffc29e404243
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-04-30 21:55:41 +0200

    Create UseCaseApplication.php

---
+++ restaurant/src/Application/UseCaseApplication.php
@@ -0,0 +1,12 @@
+<?php
+
+// UseCaseApplication.php
+
+/*
+
+This file serves as the entry point into the application:
+ 
+1. It is registered in ContainerBuilderFactory.php
+2. It can be called by the containerBuilder in the app.php file.
+
+*/
