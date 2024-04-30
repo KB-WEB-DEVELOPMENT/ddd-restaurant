49490453dd3aeb58f54a6926d5920584b3a9997f
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-04-30 22:10:01 +0200

    Create MealItem.php

---
+++ restaurant/src/Domain/Model/Meal/MealItem.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Kami\Restaurant\Domain\Model\Meal;
+
+use Kami\Restaurant\Domain\DomainException\InvalidMealProductIdException;
+use Kami\Restaurant\Domain\DomainException\InvalidQuantityException;
+
+use Ramsey\Uuid\UuidInterface;
+use Ramsey\Uuid\Uuid;
+
+final class MealItem
+{ 
+    private readonly UuidInterface $id;
+	
+    public function __construct( 	
+		private readonly string $mealProductId,
+		private int $quantity
+	)
+	
+    {
+		try {
+			if (strlen(trim($mealProductId)) == 0) {
+				throw new InvalidMealProductIdException();
+			}
+			if ($quantity <1) {
+				throw new InvalidQuantityException();
+			}
+		}	
+		
+		$this->mealProductId = $mealProductId;
+	    $this->quantity = $quantity; 	
+		$this->id = Uuid::uuid4();
+      
+	}
+
+    public function id(): UuidInterface
+    {
+        return $this->id;
+    }
+	
+    public function toString(): string
+    {
+        return ($this->id)->toString();
+    }
+		
+  	public function quantity(): int
+    {
+        return $this->quantity;
+    }
+}
