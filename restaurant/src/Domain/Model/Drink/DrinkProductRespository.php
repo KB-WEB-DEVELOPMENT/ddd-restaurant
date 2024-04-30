20d4f052748e5cd00486787977dceabe36833d8d
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-04-30 22:08:54 +0200

    Update DrinkProductRespository.php

--- restaurant/src/Domain/Model/Drink/DrinkProductRespository.php
+++ restaurant/src/Domain/Model/Drink/DrinkProductRespository.php
@@ -6,37 +6,35 @@ use Kami\Restaurant\Domain\DomainException\InvalidDrinkProductIdException;
 use Kami\Restaurant\Domain\DomainException\InvalidProductNameException;
 use Kami\Restaurant\Domain\DomainException\InvalidPriceException;
 
-
 interface DrinkProductRepository
 { 
-	/**
+    /**
     * @throws InvalidProductNameException
- 	* @throws InvalidPriceException
+    * @throws InvalidPriceException
     */	
-	public function store(string $name,int|float $price): void 
+     public function store(string $name,int|float $price): void 
 	
-	public static function all(): array
+     public static function all(): array
 	
-	/**
+    /**
     * @throws InvalidDrinkProductIdException
     */
-	public function byId(string $drinkProductId): ?DrinkProduct
+    public function byId(string $drinkProductId): ?DrinkProduct
 
-	/**
+    /**
     * @throws InvalidDrinkProductIdException
     */	
-	public function destroy(string $drinkProductId): void
+    public function destroy(string $drinkProductId): void
 	
-	/**
-	* @throws InvalidDrinkProductIdException
+    /**
+    * @throws InvalidDrinkProductIdException
     * @throws InvalidProductNameException
     */
-	public function updateName(string $drinkProductId,string $name): void
+     public function updateName(string $drinkProductId,string $name): void
 	
-	/**
+    /**
     * @throws InvalidDrinkProductIdException
-	* @throws InvalidPriceException
+    * @throws InvalidPriceException
     */
-	public function updatePrice(string $drinkProductId,int|float $price): void
-		
+    public function updatePrice(string $drinkProductId,int|float $price): void		
 }
