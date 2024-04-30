68c16fc3f434c7cc95aae4853035df10dadba3e4
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-04-30 23:18:38 +0200

    Update InMemoryDrinkProductRepository.php

--- restaurant/src/Infrastructure/InMemoryDrinkProductRepository.php
+++ restaurant/src/Infrastructure/InMemoryDrinkProductRepository.php
@@ -16,157 +16,136 @@ use Kami\Restaurant\Domain\Model\Drink\DrinkProductRepository;
 
 final class InMemoryDrinkProductRepository implements DrinkProductRepository
 { 
-
-	private array $drinkProducts = [];
+    private array $drinkProducts = [];
 		
-	public function __construct(
-	){}
-	
-	
+    public function __construct(){	    
+    }
+		
     /**
     * @throws InvalidProductNameException
- 	* @throws InvalidPriceException
+    * @throws InvalidPriceException
     */	
-	public function store(string $name,int|float $price): void
+    public function store(string $name,int|float $price): void
     {
-		try {
-			if (strlen(trim($name)) == 0) {
-				
-				throw new InvalidProductNameException();
-			}
-			if ($price <= 0) {
-				
-				throw new InvalidPriceException();  
-			}	
-		}
-
-		$cost = is_int($price) ? new Price($price) : new Price((string)$price);
-		
-		$drinkProduct = new DrinkProduct($name,$cost);
-		
-		$drinkProduct->id =  Uuid::uuid4();
-		
-		$this->drinkProducts[] = $drinkProduct; 
-		
-	}		
+       try {
+	  if (strlen(trim($name)) == 0) {			
+	     throw new InvalidProductNameException();
+	   }
+	  if ($price <= 0) {			
+	     throw new InvalidPriceException();  
+	  }	
+       }
 	
-	public static function all(): array
+       $cost = is_int($price) ? new Price($price) : new Price((string)$price);
+		
+       $drinkProduct = new DrinkProduct($name,$cost);
+		
+       $drinkProduct->id =  Uuid::uuid4();
+		
+       $this->drinkProducts[] = $drinkProduct; 		
+    }		
+	
+    public static function all(): array
     {
-		return $this->drinkProducts;
-	}
+      return $this->drinkProducts;
+    }
 
     /**
     * @throws InvalidDrinkProductIdException
     */
     public function byId(string $drinkProductId): ?DrinkProduct
     {		
-		$drinkProductIds = [];
+      $drinkProductIds = [];
 		
-		$drinkProductIds = array_map(fn($drinkProduct) => $drinkProduct->id(),$this->drinkProducts);
+      $drinkProductIds = array_map(fn($drinkProduct) => $drinkProduct->id(),$this->drinkProducts);
 	
-		try {
-			if (!in_array($drinkProductId,$drinkProductIds)) {
-				
-				throw new InvalidDrinkProductIdException(); 
-			}	
-		}
+      try {
+	if (!in_array($drinkProductId,$drinkProductIds)) {			
+           throw new InvalidDrinkProductIdException(); 
+	}	
+      }
 		
-		if (($key = array_search($drinkProductId,$drinkProductIds)) !== false) {
-			
-			return ($this->drinkProducts[$key] instanceof DrinkProduct) ? $this->drinkProducts[$key] : null;
-		
-		} 
-				
-	}
+      if (($key = array_search($drinkProductId,$drinkProductIds)) !== false) {
+	   return ($this->drinkProducts[$key] instanceof DrinkProduct) ? $this->drinkProducts[$key] : null;	
+      } 				
+   }
 
     /**
     * @throws InvalidDrinkProductIdException
     */
-	public function destroy(string $drinkProductId): void
-	{	
-		$drinkProductIds = [];
+    public function destroy(string $drinkProductId): void
+    {	
+	 $drinkProductIds = [];
 				
-		$drinkProductIds = array_map(fn($drinkProduct) => $drinkProduct->id(),$this->drinkProducts);
+	 $drinkProductIds = array_map(fn($drinkProduct) => $drinkProduct->id(),$this->drinkProducts);
 	
-		try {
-			if (!in_array($drinkProductId,$drinkProductIds)) {
-				
-				throw new InvalidDrinkProductIdException(); 
-			}	
-		}
+	 try {
+	   if (!in_array($drinkProductId,$drinkProductIds)) {			
+	     throw new InvalidDrinkProductIdException(); 
+	   }	
+	 }
 		
-		if (($key = array_search($drinkProductId,$drinkProductIds)) !== false) {
-			
-			unset($this->drinkProducts[$key]);
-		}
-			
-	}
+	if (($key = array_search($drinkProductId,$drinkProductIds)) !== false) {		
+	   unset($this->drinkProducts[$key]); 
+	}			
+    }
 	
-	/**
+    /**
     * @throws InvalidDrinkProductIdException
- 	* @throws InvalidProductNameException
+    * @throws InvalidProductNameException
     */	
-	public function updateName(string $drinkProductId,string $name): void
-	{
-		$drinkProductIds = [];
-		
-		$drinkProductIds = array_map(fn($drinkProduct) => $drinkProduct->id(),$this->drinkProducts);
-	
-		try {
-				if (!in_array($drinkProductId,$drinkProductIds)) {
-					
-					throw new InvalidDrinkProductIdException(); 
-				}
-
-				if (strlen(trim($name)) == 0) {
-					
-					throw new InvalidProductNameException();
-				}		
-		}
-		
-		if (($key = array_search($drinkProductId,$drinkProductIds)) !== false) {
-		
-			$drinkProduct = $this->drinkProducts[$key];
-			
-			$drinkProduct->name = $name;
-			
-			$this->drinkProducts[$key] = $drinkProduct; 		
-		}		
-	}
-	
-	/**
-    * @throws InvalidDrinkProductIdException
- 	* @throws InvalidPriceException
-    */
-	public function updatePrice(string $drinkProductId,int|float $price): void
+    public function updateName(string $drinkProductId,string $name): void
     {
+       $drinkProductIds = [];
 		
-		$drinkProductIds = [];
-		
-		$drinkProductIds = array_map(fn($drinkProduct) => $drinkProduct->id(),$this->drinkProducts);
+       $drinkProductIds = array_map(fn($drinkProduct) => $drinkProduct->id(),$this->drinkProducts);
 	
-		try {
-				if (!in_array($drinkProductId,$drinkProductIds)) {
-					
-					throw new InvalidDrinkProductIdException(); 
-				}
-
-				if ($price <= 0) {
-				
-					throw new InvalidPriceException();  
-			
-				}		
-		}
+       try {
+	  if (!in_array($drinkProductId,$drinkProductIds)) {				
+	       throw new InvalidDrinkProductIdException(); 
+	   }
+       	  if (strlen(trim($name)) == 0) {				
+	       throw new InvalidProductNameException();
+       	  }		
+       }
 		
-		if (($key = array_search($dealProductId,$drinkProductIds)) !== false) {
+       if (($key = array_search($drinkProductId,$drinkProductIds)) !== false) {
 		
-			$drinkProduct = $this->drinkProducts[$key];
+		$drinkProduct = $this->drinkProducts[$key];
 			
-			$drinkProduct->price = is_int($price) ? new Price($price) : new Price((string)$price);;
+		$drinkProduct->name = $name;
 			
-			$this->drinkProducts[$key] = $drinkProduct; 		
-		}
-	}
-
+		$this->drinkProducts[$key] = $drinkProduct; 		
+	}		
+     }
+	
+    /**
+    * @throws InvalidDrinkProductIdException
+    * @throws InvalidPriceException
+    */
+    public function updatePrice(string $drinkProductId,int|float $price): void
+    {		
+      $drinkProductIds = [];
+		
+      $drinkProductIds = array_map(fn($drinkProduct) => $drinkProduct->id(),$this->drinkProducts);
+	
+      try {
+	 if (!in_array($drinkProductId,$drinkProductIds)) {				
+	    throw new InvalidDrinkProductIdException(); 
+         }
+	 if ($price <= 0) {
+	    throw new InvalidPriceException();  		
+	 }		
+      }
+		
+      if (($key = array_search($dealProductId,$drinkProductIds)) !== false) {	
+         
+	   $drinkProduct = $this->drinkProducts[$key];
+			
+	   $drinkProduct->price = is_int($price) ? new Price($price) : new Price((string)$price);;
+			
+	   $this->drinkProducts[$key] = $drinkProduct; 		
+       }
+    }
 }
 
