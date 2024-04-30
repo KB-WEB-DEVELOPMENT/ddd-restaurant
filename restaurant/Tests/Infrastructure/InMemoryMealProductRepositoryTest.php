149bc2537b7a5ee1d5772e3a927438554c7edec7
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-05-01 00:20:38 +0200

    Update InMemoryMealProductRepositoryTest.php

--- restaurant/Tests/Infrastructure/InMemoryMealProductRepositoryTest.php
+++ restaurant/Tests/Infrastructure/InMemoryMealProductRepositoryTest.php
@@ -15,177 +15,174 @@ use PHPUnit\Framework\TestCase;
 
 final class InMemoryMealProductRepositoryTest extends TestCase
 {
-	private array $mealProducts = [];
-	private ?InMemoryMealProductRepository $repo = null;
+   private array $mealProducts = [];
+   private ?InMemoryMealProductRepository $repo = null;
 	
-	protected function setUp(): void
-    {
+   protected function setUp(): void
+   {
         $this->mealProducts[] = new MealProduct('Meal1',9.99);
-		$this->mealProducts[] = new MealProduct('Meal2',15.99);		
-		$this->mealProducts[] = new MealProduct('Meal3',17.99);
+        $this->mealProducts[] = new MealProduct('Meal2',15.99);		
+	$this->mealProducts[] = new MealProduct('Meal3',17.99);
 	
-		$this->repo = new InMemoryMealProductRepository();
+	$this->repo = new InMemoryMealProductRepository();
 		
-		($this->repo)->mealProducts = $this->mealProducts;
-	}
+	($this->repo)->mealProducts = $this->mealProducts;
+   }
 		
-	public function newMealProduct(): void
-    {
-		($this->repo)->store('Meal4',24.99);
+  public function newMealProduct(): void
+  {
+     ($this->repo)->store('Meal4',24.99);
 		
-		$mealProductsArray = [];
-		$mealProductsArray = ($this->repo)->mealProducts; 
+     $mealProductsArray = [];
+     $mealProductsArray = ($this->repo)->mealProducts; 
 	
-		$this->assertContainsOnlyInstancesOf(MealProduct::class,$mealProductsArray);
-	}
+     $this->assertContainsOnlyInstancesOf(MealProduct::class,$mealProductsArray);
+  }
     
-	public function newMealProductWrongName(): void
-    {
-		$this->expectException(InvalidProductNameException::class);
-		($this->repo)->store('',14.99);
-    } 
+ public function newMealProductWrongName(): void
+ {
+    $this->expectException(InvalidProductNameException::class);
+    ($this->repo)->store('',14.99);
+ } 
 
-	public function newMealProductWrongPrice(): void
-    {
-		$this->expectException(InvalidPriceException::class);
-		($this->repo)->store('Meal5',0);
-    }
+ public function newMealProductWrongPrice(): void
+ {
+    $this->expectException(InvalidPriceException::class);
+    ($this->repo)->store('Meal5',0);
+ }
 	
-	public function allMealProducts(): void
-    {
-		$mealProductsArray = [];
-		$mealProductsArray = ($this->repo)::all();
+ public function allMealProducts(): void
+ {
+   $mealProductsArray = [];
+   $mealProductsArray = ($this->repo)::all();
  		
-		$this->assertCount(3,$mealProductsArray); 
-    }
+   $this->assertCount(3,$mealProductsArray); 
+ }
 	
-	public function mealProductById(): void
-    {
-		$mealProduct = $this->mealProducts[0];
-		$uuidString = $mealProduct->id();
+public function mealProductById(): void
+{
+    $mealProduct = $this->mealProducts[0];
+    $uuidString = $mealProduct->id();
 		
-		$foundMealProduct = ($this->repo)->byId($uuidString);
+    $foundMealProduct = ($this->repo)->byId($uuidString);
 		
-		$exp = [];
-		$exp = [$foundMealProduct->name(),$foundMealProduct->priceInt()];
+    $exp = [];
+    $exp = [$foundMealProduct->name(),$foundMealProduct->priceInt()];
 		
-		$this->assertSame(['Meal1',999],$exp);
+    $this->assertSame(['Meal1',999],$exp);
 		
-    }
+}
     
-	public function mealProductByWrongId(): void
-    {
-		$this->expectException(InvalidMealProductIdException::class);
+public function mealProductByWrongId(): void
+{
+   $this->expectException(InvalidMealProductIdException::class);
 		
-		$randomUuidsStrings = [];
+   $randomUuidsStrings = [];
 				
-		$randomUuidsStrings = [
-			'a8c3de3d-1fea-4d7c-b8b0-29f63c4c3451',
-			'b8c3de3d-1fee-4d7c-b8b0-29f63c4c3452',
-			'c8c3de3d-1fea-4d7c-b8b0-29f63c8c3453',
-			'd8c3se3d-1fea-4d7c-b8b0-29f63c4c3454'
-		]
+   $randomUuidsStrings = [
+	'a8c3de3d-1fea-4d7c-b8b0-29f63c4c3451',
+	'b8c3de3d-1fee-4d7c-b8b0-29f63c4c3452',
+	'c8c3de3d-1fea-4d7c-b8b0-29f63c8c3453',
+	'd8c3se3d-1fea-4d7c-b8b0-29f63c4c3454'
+   ];
 		
-		$randomUuidString = $randomUuidsStrings[array_rand($randomUuidsStrings,1)];
+  $randomUuidString = $randomUuidsStrings[array_rand($randomUuidsStrings,1)];
 		
-		$foundMealProduct = ($this->repo)->byId($randomUuidString);
+  $foundMealProduct = ($this->repo)->byId($randomUuidString);
 		
-    } 
+} 
 
-	public function removeMealProduct(): void
-    {
-		$mealProduct = $this->mealProducts[0];
-		$uuidString = $mealProduct->id();
+public function removeMealProduct(): void
+{
+  $mealProduct = $this->mealProducts[0];
+  
+  $uuidString = $mealProduct->id();
 		
-		($this->repo)->destroy($uuidString);
+  ($this->repo)->destroy($uuidString);
 		
-		$mealProductsArr = [];
+  $mealProductsArr = [];
 		
-		$mealProductsArr = ($this->repo)->mealProducts; 
+  $mealProductsArr = ($this->repo)->mealProducts; 
 		
-		$count = count($mealProductsArr);
+  $count = count($mealProductsArr);
  		
-		$this->assertSame(2,$count); 
-		
-    }
+  $this->assertSame(2,$count); 		
+}
 	
-	public function removeWrongMealProduct(): void
-    {
-		
-		$this->expectException(InvalidMealProductIdException::class);
+public function removeWrongMealProduct(): void
+{		
+   $this->expectException(InvalidMealProductIdException::class);
 			
-		$randomUuidsStrings = [];
+   $randomUuidsStrings = [];
 			
-		$randomUuidsStrings = [
-			'a8c3de3d-1fea-4d7c-b8b0-29f63c4c3451',
-			'b8c3de3d-1fee-4d7c-b8b0-29f63c4c3452',
-			'c8c3de3d-1fea-4d7c-b8b0-29f63c8c3453',
-			'd8c3se3d-1fea-4d7c-b8b0-29f63c4c3454'
-		]
+   $randomUuidsStrings = [
+       'a8c3de3d-1fea-4d7c-b8b0-29f63c4c3451',
+	'b8c3de3d-1fee-4d7c-b8b0-29f63c4c3452',
+	'c8c3de3d-1fea-4d7c-b8b0-29f63c8c3453',
+	'd8c3se3d-1fea-4d7c-b8b0-29f63c4c3454'
+  ];
 		
-		$randomUuidString = $randomUuidsStrings[array_rand($randomUuidsStrings,1)];
+   $randomUuidString = $randomUuidsStrings[array_rand($randomUuidsStrings,1)];
 		
-		($this->repo)->destroy($randomUuidString);
-    }
+   ($this->repo)->destroy($randomUuidString);
+}
 	
-	public function updateMealProductByPrice(): void
-    {
-		$mealProduct = $this->mealProducts[2];
+public function updateMealProductByPrice(): void
+{
+   $mealProduct = $this->mealProducts[2];
 		
-		$uuidString = $mealProduct->id();
+   $uuidString = $mealProduct->id();
 		
-		($this->repo)->updatePrice($uuidString,39.99);
+   ($this->repo)->updatePrice($uuidString,39.99);
 		
-		$changedMealProduct = ($this->repo)->mealProducts[2];
+   $changedMealProduct = ($this->repo)->mealProducts[2];
 		
-		$updatedPriceInt = $changedMealProduct->priceInt(); 
+   $updatedPriceInt = $changedMealProduct->priceInt(); 
 				
-		$this->assertSame(3999,$updatedPriceInt); 
-		
-    }
+   $this->assertSame(3999,$updatedPriceInt); 		
+}
     
-	public function updateMealProductByWrongPrice(): void
-    {
-		$this->expectException(InvalidPriceException::class);
+public function updateMealProductByWrongPrice(): void
+{
+   $this->expectException(InvalidPriceException::class);
 		
-		$mealProduct = $this->mealProducts[2];
+   $mealProduct = $this->mealProducts[2];
 		
-		$uuidString = $mealProduct->id();
+   $uuidString = $mealProduct->id();
 		
-		($this->repo)->updatePrice($uuidString,-5);
-    } 
+   ($this->repo)->updatePrice($uuidString,-5);
+} 
 
-	public function updateMealProductByName(): void
-    {
-		$mealProduct = $this->mealProducts[2];
+public function updateMealProductByName(): void
+{
+   $mealProduct = $this->mealProducts[2];
 		
-		$uuidString = $mealProduct->id();
+   $uuidString = $mealProduct->id();
 		
-		($this->repo)->updateName($uuidString,'Meal9');
+   ($this->repo)->updateName($uuidString,'Meal9');
 		
-		$changedMealProduct = ($this->repo)->mealProducts[2];
+   $changedMealProduct = ($this->repo)->mealProducts[2];
 		
-		$updatedName = $changedMealProduct->name(); 
+   $updatedName = $changedMealProduct->name(); 
 				
-		$this->assertSame('Meal9',$updatedName); 
-		
-    }
+   $this->assertSame('Meal9',$updatedName); 		
+}
 	
-	public function updateMealProductByWrongName(): void
-    {
-		$this->expectException(InvalidProductNameException::class);
+public function updateMealProductByWrongName(): void
+{
+	$this->expectException(InvalidProductNameException::class);
 		
-		$mealProduct = $this->mealProducts[2];
+	$mealProduct = $this->mealProducts[2];
 		
-		$uuidString = $mealProduct->id();
+	$uuidString = $mealProduct->id();
 		
-		($this->repo)->updateName($uuidString,'');		
-    }
+	($this->repo)->updateName($uuidString,'');		
+}
 	
-	protected function tearDown(): void
-    {
-        unset($this->mealProducts);
+ protected function tearDown(): void
+ {
+   unset($this->mealProducts);
 		
-		$this->repo = null;
-    }
+   $this->repo = null;
+ }
 }
