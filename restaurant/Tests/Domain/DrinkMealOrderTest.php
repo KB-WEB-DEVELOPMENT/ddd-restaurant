b09f5f70b5327e39b7c4716a292fd245d8462fa0
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-04-30 23:59:17 +0200

    Update DrinkMealOrderTest.php

--- restaurant/Tests/Domain/DrinkMealOrderTest.php
+++ restaurant/Tests/Domain/DrinkMealOrderTest.php
@@ -19,263 +19,251 @@ use Ramsey\Uuid\Uuid;
 
 use PHPUnit\Framework\TestCase;
 
-
 final class DrinkMealOrderTest extends TestCase
 {
-	private ?DrinkMealOrder $drinkMealOrder = null;
+  private ?DrinkMealOrder $drinkMealOrder = null;
+  private ?InMemoryMealProductRepository $inMemoryMealRepo = null;
+  private ?InMemoryDrinkProductRepository $inMemoryDrinklRepo = null;
 	
-	private ?InMemoryMealProductRepository $inMemoryMealRepo = null;
-	private ?InMemoryDrinkProductRepository $inMemoryDrinklRepo = null;
-	
-	protected function setUp(): void
-    {
-		$this->inMemoryMealRepo = new InMemoryMealProductRepository();
+  protected function setUp(): void
+  {
+    $this->inMemoryMealRepo = new InMemoryMealProductRepository();
 		
-		$this->inMemoryDrinkRepo = new InMemoryDrinkProductRepository();
+    $this->inMemoryDrinkRepo = new InMemoryDrinkProductRepository();
 
-		($this->inMemoryMealRepo)->store('Meal1',9.15);
-		($this->inMemoryMealRepo)->store('Meal2',19.75);
-		($this->inMemoryMealRepo)->store('Meal3',29.99);
+   ($this->inMemoryMealRepo)->store('Meal1',9.15);
+   ($this->inMemoryMealRepo)->store('Meal2',19.75);
+   ($this->inMemoryMealRepo)->store('Meal3',29.99);
 		
-		($this->inMemoryDrinkRepo)->store('Drink1',7.55);
-		($this->inMemoryDrinkRepo)->store('Drink2',14.65);
-		($this->inMemoryDrinkRepo)->store('Drink3',20.10);
+   ($this->inMemoryDrinkRepo)->store('Drink1',7.55);
+   ($this->inMemoryDrinkRepo)->store('Drink2',14.65);
+   ($this->inMemoryDrinkRepo)->store('Drink3',20.10);
 		
-		$this->drinkMealOrder = new DrinkMealOrder(
-									$this->inMemoryMealRepo,
-							        $this->inMemoryDrinkRepo
-								);		
-	}
+   $this->drinkMealOrder = new DrinkMealOrder(
+                                  $this->inMemoryMealRepo,
+				  $this->inMemoryDrinkRepo
+			   );		
+ }
 		
-	public function newEmptyDrinkMealOrder(): void
-    {
-		$mealItemsArr = [];
+ public function newEmptyDrinkMealOrder(): void
+ {
+    $mealItemsArr = [];
 		
-		$mealItemsArr = ($this->drinkMealOrder)->mealItems;
+    $mealItemsArr = ($this->drinkMealOrder)->mealItems;
 		
-		$this->assertEmpty($mealItemsArr);	
-	}
+    $this->assertEmpty($mealItemsArr);	
+ }
 		
-	// same for addDrinkProductItem(): void { ... }
-	public function addMealProductItem(): void
-    {
-		$allMealProductsArr = [];
+// same for addDrinkProductItem(): void { ... }
+public function addMealProductItem(): void
+{
+   $allMealProductsArr = [];
 		
-		$allMealProductIdsArr = [];
+   $allMealProductIdsArr = [];
 		
-		$mealItemsArr = [];
+   $mealItemsArr = [];
 		
-		$allMealProductsArr = ($this->inMemoryMealRepo)::all();
+   $allMealProductsArr = ($this->inMemoryMealRepo)::all();
 		
-        $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr); 		
+   $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr); 		
 	
-		$randomMealProductId = $allMealProductIdsArr[array_rand($allMealProductIdsArr,1)];
+   $randomMealProductId = $allMealProductIdsArr[array_rand($allMealProductIdsArr,1)];
 				
-		$randomQuantity = rand(1,5);
+   $randomQuantity = rand(1,5);
 		
-		$newMealItem = new MealItem($randomMealProductId,$randomQuantity);
+   $newMealItem = new MealItem($randomMealProductId,$randomQuantity);
 		
-		($this->drinkMealOrder)->addMealItem($newMealItem);
+   ($this->drinkMealOrder)->addMealItem($newMealItem);
 	
-		$mealItemsArr = ($this->drinkMealOrder)->mealItems;
+   $mealItemsArr = ($this->drinkMealOrder)->mealItems;
 		
-		$newestMealItem = end($mealItemsArr);
+   $newestMealItem = end($mealItemsArr);
 		
-		$this->assertContainsOnlyInstancesOf(MealItem::class,[$newestMealItem]);
+   $this->assertContainsOnlyInstancesOf(MealItem::class,[$newestMealItem]);
 	
-	}
+}
 	
-	// same for addNonExistingDrinkProductItem(): void { ... }
-	public function addNonExistingMealProductItem(): void
-    {
-		$this->expectException(InvalidMealProductIdException::class);
+// same for addNonExistingDrinkProductItem(): void { ... }
+public function addNonExistingMealProductItem(): void
+{
+    $this->expectException(InvalidMealProductIdException::class);
 
-		$nonExistingMealProductId =  Uuid::uuid4()->toString();
+    $nonExistingMealProductId =  Uuid::uuid4()->toString();
 		
-		$randomQuantity = rand(1,5);
+    $randomQuantity = rand(1,5);
 		
-		$newMealItem = new MealItem($nonExistingMealProductId,$randomQuantity);
+    $newMealItem = new MealItem($nonExistingMealProductId,$randomQuantity);
 		
-		($this->drinkMealOrder)->addMealItem($newMealItem);
-    }
+    ($this->drinkMealOrder)->addMealItem($newMealItem);
+}
 	
-	// same for removeDrinkProductItem(): void { ... }		
-	public function removeMealProductItem(): void
-    {		
-		$allMealProductsArr = [];
+// same for removeDrinkProductItem(): void { ... }		
+public function removeMealProductItem(): void
+{		
+    $allMealProductsArr = [];
 		
-		$allMealProductIdsArr = [];
+    $allMealProductIdsArr = [];
 		
-		$mealItemsArr = [];
+    $mealItemsArr = [];
 		
-		$allMealProductsArr = ($this->inMemoryMealRepo)::all();
+    $allMealProductsArr = ($this->inMemoryMealRepo)::all();
 		
-        $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr); 		
+    $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr); 		
 	
-		$randomMealProductId = $allMealProductIdsArr[array_rand($allMealProductIdsArr,1)];
+    $randomMealProductId = $allMealProductIdsArr[array_rand($allMealProductIdsArr,1)];
 				
-		$randomQuantity = rand(1,5);
+    $randomQuantity = rand(1,5);
 		
-		$newMealItem = new MealItem($randomMealProductId,$randomQuantity);
+    $newMealItem = new MealItem($randomMealProductId,$randomQuantity);
 		
-		($this->drinkMealOrder)->addMealItem($newMealItem);
+    ($this->drinkMealOrder)->addMealItem($newMealItem);
 		
-		$mealItemId = $newMealItem->toString(); 
+    $mealItemId = $newMealItem->toString(); 
 		
-		($this->drinkMealOrder)->removeMealItem($mealItemId);
+    ($this->drinkMealOrder)->removeMealItem($mealItemId);
 		
-		$mealItemsArr = ($this->drinkMealOrder)->mealItems;
+    $mealItemsArr = ($this->drinkMealOrder)->mealItems;
 		
-		$this->assertEmpty($mealItemsArr);	
-					
-    }
-	// same for removeNonExistingDrinkProductItem(): void { ... }	
-	public function removeNonExistingMealProductItem(): void
-    {
-		$this->expectException(InvalidMealItemIdException::class);
+    $this->assertEmpty($mealItemsArr);						
+}
+	
+// same for removeNonExistingDrinkProductItem(): void { ... }	
+public function removeNonExistingMealProductItem(): void
+{
+   $this->expectException(InvalidMealItemIdException::class);
 		 
-		$mealItemId = 'invalid';
+   $mealItemId = 'invalid';
 		
-		($this->drinkMealOrder)->removeMealItem($mealItemId);
-    }
+   ($this->drinkMealOrder)->removeMealItem($mealItemId);
+}
 	
-	// same for emptyAllDrinkProductItems(): void { ... }	
-	public function emptyAllMealProductItems(): void
-    {
-		$mealItems = [];
+// same for emptyAllDrinkProductItems(): void { ... }	
+public function emptyAllMealProductItems(): void
+{
+   $mealItems = [];
 			
-		($this->drinkMealOrder)->removeAllMealItems();
+   ($this->drinkMealOrder)->removeAllMealItems();
 			
-		$mealItems = ($this->drinkMealOrder)->mealItems;
+   $mealItems = ($this->drinkMealOrder)->mealItems;
 
-		$this->assertEmpty($mealItems);		
-   
-	}	
+   $this->assertEmpty($mealItems);		   
+}	
 	
-	public function emptyItems(): void
-    {
-		$mealItems = [];
-		$drinkItems = [];
+public function emptyItems(): void
+{
+  $mealItems = [];
+  $drinkItems = [];
     
-		($this->drinkMealOrder)->removeAllSustenanceItems();
+  ($this->drinkMealOrder)->removeAllSustenanceItems();
 		
-		$mealItems = ($this->drinkMealOrder)->mealItems;
+  $mealItems = ($this->drinkMealOrder)->mealItems;
 		
-		$drinkItems = ($this->drinkMealOrder)->drinkItems;
+  $drinkItems = ($this->drinkMealOrder)->drinkItems;
 		
-		$merged = array_merge($mealItems,$drinkItems);
+  $merged = array_merge($mealItems,$drinkItems);
 	
-		$this->assertEmpty($merged);
-	}
+  $this->assertEmpty($merged);
+}
 		
-	// same for calculateDrinkProductItemsCostNoVat(): void { ... }
-	public function calculateMealProductItemsCostNoVat(): void
-    {
-		$allMealProductsArr = [];
+// same for calculateDrinkProductItemsCostNoVat(): void { ... }
+public function calculateMealProductItemsCostNoVat(): void
+{
+  $allMealProductsArr = [];
 		
-		$allMealProductIdsArr = [];
+  $allMealProductIdsArr = [];
 								
-		$allMealProductsArr = ($this->inMemoryMealRepo)::all();
+  $allMealProductsArr = ($this->inMemoryMealRepo)::all();
 		
-        $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr); 		
+  $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr); 		
 				
-		foreach ($allMealProductIdsArr as $key => $mealProductId) {
-			
-			($this->drinkMealOrder)->mealItems[] = new MealItem($mealProductId,1);
-		}
+  foreach ($allMealProductIdsArr as $key => $mealProductId) {			
+      ($this->drinkMealOrder)->mealItems[] = new MealItem($mealProductId,1);
+  }
 
-		$costInt = ($this->drinkMealOrder)->mealItemsCostNoVat();
+  $costInt = ($this->drinkMealOrder)->mealItemsCostNoVat();
 		
-		$this->assertSame(5889,$costInt);
+  $this->assertSame(5889,$costInt);		
+}
+
+// same for calculateDrinkProductItemsCostWithVat(): void { ... }
+public function calculateMealProductItemsCostWithVat(): void
+{
+   $allMealProductsArr = [];
 		
+   $allMealProductIdsArr = [];
+								
+   $allMealProductsArr = ($this->inMemoryMealRepo)::all();
+		
+   $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr); 		
+				
+   foreach ($allMealProductIdsArr as $key => $mealProductId) {			
+        ($this->drinkMealOrder)->mealItems[] = new MealItem($mealProductId,1);
     }
 
-	// same for calculateDrinkProductItemsCostWithVat(): void { ... }
-	public function calculateMealProductItemsCostWithVat(): void
-    {
-		$allMealProductsArr = [];
+    $costInt = ($this->drinkMealOrder)->mealItemsCostWithVat();
 		
-		$allMealProductIdsArr = [];
+    $this->assertSame(7007,$costInt);		
+  }
+
+public function calculateTotalCostInt(): void
+{
+  $allMealProductsArr = [];
+  $allDrinkProductsArr = [];
+		
+  $allMealProductIdsArr = [];
+  $allDrinkProductIdsArr = [];
 								
-		$allMealProductsArr = ($this->inMemoryMealRepo)::all();
+  $allMealProductsArr =  ($this->inMemoryMealRepo)::all();
+  $allDrinkProductsArr = ($this->inMemoryDrinkRepo)::all();
 		
-        $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr); 		
+  $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr);
+  $allDrinkProductIdsArr = array_map(fn($drinkProduct) => $drinkProduct->id(),$allDrinkProductsArr);	
 				
-		foreach ($allMealProductIdsArr as $key => $mealProductId) {
-			
-			($this->drinkMealOrder)->mealItems[] = new MealItem($mealProductId,1);
-		}
+  foreach ($allMealProductIdsArr as $key => $mealProductId) {			
+	($this->drinkMealOrder)->mealItems[] = new MealItem($mealProductId,1);
+  }
+		
+  foreach ($allDrinkProductIdsArr as $key => $drinkProductId) {			
+  	($this->drinkMealOrder)->drinkItems[] = new DrinkItem($drinkProductId,1);
+  }
 
-		$costInt = ($this->drinkMealOrder)->mealItemsCostWithVat();
-		
-		$this->assertSame(7007,$costInt);
-		
-    }
+  $costInt = ($this->drinkMealOrder)->totalCostInt();
 
-	public function calculateTotalCostInt(): void
-    {
-		$allMealProductsArr = [];
-		$allDrinkProductsArr = [];
+  $this->assertSame(12040,$costInt);				
+}
+
+public function calculateTotalCostFormatted(): void
+{		
+  $allMealProductsArr = [];
+  $allDrinkProductsArr = [];
 		
-		$allMealProductIdsArr = [];
-		$allDrinkProductIdsArr = [];
+  $allMealProductIdsArr = [];
+  $allDrinkProductIdsArr = [];
 								
-		$allMealProductsArr =  ($this->inMemoryMealRepo)::all();
-		$allDrinkProductsArr = ($this->inMemoryDrinkRepo)::all();
+  $allMealProductsArr =  ($this->inMemoryMealRepo)::all();
+  $allDrinkProductsArr = ($this->inMemoryDrinkRepo)::all();
 		
-        $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr);
-		$allDrinkProductIdsArr = array_map(fn($drinkProduct) => $drinkProduct->id(),$allDrinkProductsArr);	
+  $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr);
+  $allDrinkProductIdsArr = array_map(fn($drinkProduct) => $drinkProduct->id(),$allDrinkProductsArr);	
 				
-		foreach ($allMealProductIdsArr as $key => $mealProductId) {
-			
-			($this->drinkMealOrder)->mealItems[] = new MealItem($mealProductId,1);
-		}
+  foreach ($allMealProductIdsArr as $key => $mealProductId) { 			
+     ($this->drinkMealOrder)->mealItems[] = new MealItem($mealProductId,1);
+  }
 		
-		foreach ($allDrinkProductIdsArr as $key => $drinkProductId) {
-			
-			($this->drinkMealOrder)->drinkItems[] = new DrinkItem($drinkProductId,1);
-		}
+  foreach ($allDrinkProductIdsArr as $key => $drinkProductId) {			
+	($this->drinkMealOrder)->drinkItems[] = new DrinkItem($drinkProductId,1);
+  }
 
-		$costInt = ($this->drinkMealOrder)->totalCostInt();
+ $costFormatted = ($this->drinkMealOrder)->totalCostFormatted();
 
-		$this->assertSame(12040,$costInt);		
-		
-    }
+ $this->assertSame('EUR 120.40',$costFormatted); 
+}
 
-	public function calculateTotalCostFormatted(): void
-    {		
-		$allMealProductsArr = [];
-		$allDrinkProductsArr = [];
-		
-		$allMealProductIdsArr = [];
-		$allDrinkProductIdsArr = [];
-								
-		$allMealProductsArr =  ($this->inMemoryMealRepo)::all();
-		$allDrinkProductsArr = ($this->inMemoryDrinkRepo)::all();
-		
-        $allMealProductIdsArr = array_map(fn($mealProduct) => $mealProduct->id(),$allMealProductsArr);
-		$allDrinkProductIdsArr = array_map(fn($drinkProduct) => $drinkProduct->id(),$allDrinkProductsArr);	
-				
-		foreach ($allMealProductIdsArr as $key => $mealProductId) {
-			
-			($this->drinkMealOrder)->mealItems[] = new MealItem($mealProductId,1);
-		}
-		
-		foreach ($allDrinkProductIdsArr as $key => $drinkProductId) {
-			
-			($this->drinkMealOrder)->drinkItems[] = new DrinkItem($drinkProductId,1);
-		}
-
-		$costFormatted = ($this->drinkMealOrder)->totalCostFormatted();
-
-		$this->assertSame('EUR 120.40',$costFormatted);
-    }
-
-	protected function tearDown(): void
-    {
-		unset($this->inMemoryMealRepo);
-		unset($this->inMemoryDrinkRepo);
-		unset($this->drinkMealOrder);		
-    }	
-	 
+ protected function tearDown(): void
+ {
+   unset($this->inMemoryMealRepo);
+   unset($this->inMemoryDrinkRepo);
+   unset($this->drinkMealOrder);		
+ }
+	
 }
