84d65a45123c3c971dd9289039e6b43aefe7a492
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-05-01 00:04:41 +0200

    Update MealItemTest.php

--- restaurant/Tests/Domain/MealItemTest.php
+++ restaurant/Tests/Domain/MealItemTest.php
@@ -15,54 +15,53 @@ use PHPUnit\Framework\TestCase;
 
 final class MealItemTest extends TestCase
 {
-	public function newMealItem(): void
+    public function newMealItem(): void
     {
-		$mealItem = new MealItem('111111',2);
+         $mealItem = new MealItem('111111',2);
 		
-		$this->assertContainsOnlyInstancesOf(MealItem::class,[$mealItem]);
+	 $this->assertContainsOnlyInstancesOf(MealItem::class,[$mealItem]);
+    }
+	
+    public function wrongNewMealItemProductId(): void
+    {
+         $this->expectException(InvalidMealProductIdException::class);
+		
+	 $mealItem = new MealItem('',2);
+    }
+	
+    public function wrongNewMealItemQuantity(): void
+    {
+         $this->expectException(InvalidQuantityException::class);
+		
+	 $mealItem = new MealItem('111111',-3);	
+    }
+	
+   public function testUuidObject(): void
+    {
+        $mealItem = new MealItem('111111',2);
+	$uuidObject = $mealItem->id();
+	$this->assertTrue($uuidObject instanceof TestInterface);
+    }
+	
+    public function testUuidString(): void
+    {
+      $mealItem = new MealItem('333333',1);
+		
+      $fromMethod = $mealItem->toString();
+		
+      $id = $mealItem->id();
+		
+      $fromUuid = $id->toString(); 
 
+      $this->assertSame($fromMethod,$fromUuid);			
     }
 	
-	public function wrongNewMealItemProductId(): void
+    public function testQuantity(): void
     {
-		$this->expectException(InvalidMealProductIdException::class);
+       $mealItem = new MealItem('101010',2);
 		
-		$mealItem = new MealItem('',2);
-    }
-	public function wrongNewMealItemQuantity(): void
-    {
-		$this->expectException(InvalidQuantityException::class);
+       $quantity = $mealItem->quantity();
 		
-		$mealItem = new MealItem('111111',-3);	
-    }
-	
-	public function testUuidObject(): void
-    {
-		$mealItem = new MealItem('111111',2);
-		$uuidObject = $mealItem->id();
-		$this->assertTrue($uuidObject instanceof TestInterface);
-    }
-	
-	public function testUuidString(): void
-    {
-		$mealItem = new MealItem('333333',1);
-		
-		$fromMethod = $mealItem->toString();
-		
-		$id = $mealItem->id();
-		
-		$fromUuid = $id->toString(); 
-
-		$this->assertSame($fromMethod,$fromUuid);			
-	
-	}
-	
-	public function testQuantity(): void
-    {
-		$mealItem = new MealItem('101010',2);
-		
-		$quantity = $mealItem->quantity();
-		
-		$this->assertSame(2,$quantity);
+       $this->assertSame(2,$quantity);
     }
 }
