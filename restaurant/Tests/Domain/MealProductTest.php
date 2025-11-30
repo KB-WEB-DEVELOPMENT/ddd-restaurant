a8cec8ed4acd2e35cdcf08a9834a98fd6b026931
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-11-30 11:51:48 +0100

    Update MealProductTest.php

--- restaurant/Tests/Domain/MealProductTest.php
+++ restaurant/Tests/Domain/MealProductTest.php
@@ -16,23 +16,23 @@ final class MealProductTest extends TestCase
 	
   public function newProduct(): void
   {
-    $mealProduct = new MealProduct('Meal1',9.99);
+    	$mealProduct = new MealProduct('Meal1',9.99);
 		
-    $this->assertContainsOnlyInstancesOf(MealProduct::class,[$mealProduct]);
+    	$this->assertContainsOnlyInstancesOf(MealProduct::class,[$mealProduct]);
   }
 	
   public function newWrongNameProduct(): void
   {
        $this->expectException(InvalidProductNameException::class);
         
-	$mealProduct = new MealProduct('',9.99);		
+	   $mealProduct = new MealProduct('',9.99);		
   }
 	
   public function newWrongPriceProduct(): void
   {
-    $this->expectException(InvalidPriceException::class);
+    	$this->expectException(InvalidPriceException::class);
         
-    $mealProduct = new MealProduct('Meal3',-9.99);
+    	$mealProduct = new MealProduct('Meal3',-9.99);
   }
 		
   public function updatePrice(): void
@@ -75,3 +75,4 @@ final class MealProductTest extends TestCase
     $mealProduct->changeName('');
   }	 
 }
+
